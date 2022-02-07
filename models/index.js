14e6819c4c40505a812601a8a03a22f95db7a2bc
Bradley Le <brabrele@gmail.com>
2022-02-07 03:37:05 -0800

    created associations in index.js for models

--- models/index.js
+++ models/index.js
@@ -8,10 +8,26 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'product_tags',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'tags_for_products',
+});
+
 
 module.exports = {
   Product,
