9386a21b872265f9944e1328080ad688637b0f23
Bradley Le <brabrele@gmail.com>
2022-02-07 22:34:42 -0800

    added DELETE route in product-routes.js to remove an entire entry of a product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -110,8 +110,13 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  const productData = await Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
 });
 
 module.exports = router;
