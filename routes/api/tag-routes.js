947069f54a4d60e0d31cae349121d4927349142a
Bradley Le <brabrele@gmail.com>
2022-02-07 21:18:12 -0800

    added DELETE route in tag-routes.js to remove tags via id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -59,8 +59,13 @@ router.put('/:id', async (req, res) => {
   )
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  const tagData = await Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
 });
 
 module.exports = router;
